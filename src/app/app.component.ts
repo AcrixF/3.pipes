62d6ccd0683a178a6e64356dd84e706e29db5972
AcrixF <acrixf@gmail.com>
2017-05-23 10:16:23 -0500

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {setTimeout} from "timers";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  nombre : string ="Cristian";
+
+  nombre2 : string = "criStian floRes marTinez";
+
+  arreglo = [1,2,3,4,5,6,7,8,9];
+
+  PI = Math.PI;
+
+  a:number = 0.234;
+
+
+  salario = 1234.5;
+
+  heroe = {
+
+    nombre:"Logan",
+    clave:"Wolvering",
+    edad:500,
+    direccion:{
+      calle:"Primavera",
+      casa:19
+    }
+
+  }
+
+  valorDePromesa =  new Promise((resolve,reject) => {
+    setTimeout(() => resolve('Llego la data'),3500);
+  })
+
+
+  fecha = new Date();
+
+  video = "fDtb53un3-Y";
+
+  contrasena = "contraseña";
+
+  activar:boolean= true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,29 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { LOCALE_ID } from '@angular/core';
+
 import { AppComponent } from './app.component';
+
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipePipe } from './pipes/contrasena-pipe.pipe'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipePipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+
+  /*Este vatriable permite cambiar la configuracion del idioma*/
+  providers: [{provide:LOCALE_ID,useValue:"es"}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
